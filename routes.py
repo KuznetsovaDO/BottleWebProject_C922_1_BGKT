883784594daa75ab73afcae8039a5ef4c5712d01
Kuznetsova <kuzdiana000@gmail.com>
2022-05-18 22:31:09 +0300

    Вариант 2. Реализовано вычисление методом поиска в глубину, вывод результата и сохранение матрицы пользователя

--- routes.py
+++ routes.py
@@ -2,8 +2,9 @@
 Routes and views for the bottle application.
 """
 
-from bottle import route, view, request
+from bottle import route, view, request, response
 from datetime import datetime
+import depth_first_search
 
 @route('/')
 @route('/home')
@@ -15,25 +16,60 @@ def home():
     )
 
 
-
 @route('/variant2')
 @view('variant2')
 def variant1():
    size_ = request.GET.get('SIZE')
    if (size_ != None):
+       response.set_cookie("size", size_)
        return dict(
         title='Title',
         message='Your application description page.',
         year=datetime.now().year,
-        size = int(size_)
+        size = int(size_),
+        check = 'false'
     )
    else:
+       response.set_cookie("size", "4")
        return dict(
         title='Title',
         message='Your application description page.',
         year=datetime.now().year, 
-        size = 4
+        size = 4,
+        check = 'false'
     )
+
+@route('/dfs')
+@view('variant2')
+def new():
+    size = int(request.cookies.size)
+
+    matrix = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+           row.append(str(request.GET.get(str(i)+'_'+str(j))))
+        matrix.append(row)
+
+    inc = {}
+    for i in range(size+1):
+        tops= []
+        for j in range(size+1):
+            if (str(request.GET.get(str(i)+'_'+str(j)))=='1'):
+                tops.append(j)
+            inc[i]=tops  
+    result = depth_first_search.DFS(inc)
+
+    return dict(
+        title='Title',
+        message='Your application description page.',
+        year=datetime.now().year, 
+        size = size,
+        check = 'true',
+        result = result, 
+        matrix = matrix
+    )
+
 
 @route('/variant1')
 @view('variant1')
