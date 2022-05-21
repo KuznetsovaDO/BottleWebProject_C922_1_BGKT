8c4f8a9e7566de024b95e6ef44ebdd98b9cf150c
Toma <asistnik@gmail.com>
2022-05-21 23:38:12 +0300

    Вариант 3. Оформления вывода результатов метода Крускала

--- kruskalPy.py
+++ kruskalPy.py
@@ -10,15 +10,19 @@ def kruskal_yu():
     final_ = request.forms.getall('finalVertex')
     size_ = len(weight_)
     vert_ = request.forms.get('el1', type=int)
-    errors = ""
+    errors = []
+    er = 0
+    
     # проверка на связанность графа
     if not(str(vert_) in start_):
         if not(str(vert_) in final_):
-            errors = "graf ne svyazan"
+            errors.insert(er, "ne svyazan")
+            er += 1
     # проверка на наличие петель в графе
     for i in range(size_):
         if (int(start_[i]) == int(final_[i])):
-            errors = "vvedite znachenya bez petel'"
+            errors.insert(er, "petlya")
+            er += 1
     # проверка на наличие повторяющихся ребер
     for i in range(size_):
         for j in range(size_):
@@ -28,7 +32,8 @@ def kruskal_yu():
             duo22 =  final_[j] + start_[j]
             if i != j:
                 if duo11 == duo21 or duo11 == duo22 or duo12 == duo21 or duo12 == duo22:
-                    errors = "povtor"
+                    errors.insert(er, "povtor")
+                    er += 1
     R = [[]]
     # заполнение массива
     for i in range(size_):
@@ -39,30 +44,50 @@ def kruskal_yu():
     U = set()   # список соединенных вершин
     D = {}      # словарь списка изолированных групп вершин
     T = []      # список ребер остова
-    
+    proc = "Next, we will create a list of isolated vertex groups to avoid the appearance of cycles. "
+    process = []
+    p = 0
     for r in Rs:
         if r[1] not in U or r[2] not in U:  # проверка для исключения циклов в остове
             if r[1] not in U and r[2] not in U: # если обе вершины не соединены, то
                 D[r[1]] = [r[1], r[2]]          # формируем в словаре ключ с номерами вершин
                 D[r[2]] = D[r[1]]               # и связываем их с одним и тем же списком вершин
+                proc = "Vertices " + str(r[1]) + " and " + str(r[2]) + " meet for the first time, forming a group of isolated vertices without creating a cycle. Therefore, they are definitely part of the spanning tree. "
+                process.insert(p, proc)
+                p += 1
             else:                           # иначе
                 if not D.get(r[1]):             # если в словаре нет первой вершины, то
                     D[r[2]].append(r[1])        # добавляем в список первую вершину
                     D[r[1]] = D[r[2]]           # и добавляем ключ с номером первой вершины
+                    proc = "The vertex " + str(r[2]) + " has already been encountered before, but the vertex " + str(r[1]) + " occurs for the first time and does not form a cycle. Therefore, the edge between these two vertices is part of the spanning tree. "
+                    process.insert(p, proc)
+                    p += 1
                 else:
                     D[r[1]].append(r[2])        # иначе, все то же самое делаем со второй вершиной
                     D[r[2]] = D[r[1]]
+                    proc = "The vertex " + str(r[1]) + " has already been encountered before, but the vertex " + str(r[2]) + " occurs for the first time and does not form a cycle. Therefore, the edge between these two vertices is part of the spanning tree. "
+                    process.insert(p, proc)
+                    p += 1
     
             T.append(r)             # добавляем ребро в остов
             U.add(r[1])             # добавляем вершины в множество U
             U.add(r[2])
-    
+        else:
+            proc = "The vertices " + str(r[1]) + " and " + str(r[2]) + " have already met before, so the edge connecting these vertices is not of interest to us at the moment. "
+            process.insert(p, proc)
+            p += 1
+    proc = "Now that we have groups of isolated vertices, we need to connect them together. To do this, once again consider the sorted table with the edges of the graph and find the edge with the lowest weight connecting the isolated groups. "
+    process.insert(p, proc)
+    p += 1
     for r in Rs:    # проходим по ребрам второй раз и объединяем разрозненные группы вершин
         if r[2] not in D[r[1]]:     # если вершины принадлежат разным группам, то объединяем
             T.append(r)             # добавляем ребро в остов
             gr1 = D[r[1]]           # сохраняем значение до объединения
             D[r[1]] += D[r[2]]      # объединем списки двух групп вершин
             D[r[2]] += gr1
+            proc = "Consider vertices " + str(r[1]) + " and " + str(r[2]) + " connected by an edge. These vertices belong to different isolated groups, therefore this edge will be part of the spanning tree. "
+            process.insert(p, proc)
+            p += 1
     return template('kruskalRez',
             title='Kruskal',
             year=datetime.now().year,
@@ -71,7 +96,8 @@ def kruskal_yu():
             U=U,
             Rs=Rs,
             D=D,
-            errors = errors)
+            errors = errors,
+            process=process)
 
 
 
--- views/kruskalRez.tpl
+++ views/kruskalRez.tpl
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
-% rebase('layout.tpl', title=title, year=year, T=T, R=R, U=U, Rs=Rs, D=D, errors=errors)
+% rebase('layout.tpl', title=title, year=year, T=T, R=R, U=U, Rs=Rs, D=D, errors=errors, process=process)
 <link href="/static/content/style.css" rel="stylesheet">
 	<div class="jumbotron">
 <body>
+<!-- форма с выводом результата по методу Крускала -->
 <form align = center>
+<!-- проверка на наличие ошибок в введенных данных -->
 % if len(errors) != 0:
-<p> {{errors}} </p>
+<p class="hhh">Error</p>
+<!-- вывод информации об ошибкая -->
+<p>When entering data, you made the following mistakes:</p>
+<!-- ошибка не связанного графа -->
+% if "ne svyazan" in errors:
+<p>The graph is not connected. Make sure that the data is correct. There is no minimum backbone in an unrelated graph.</p>
 % end
+<!-- ошибка наличия петель -->
+% if "petlya" in errors:
+<p>There is a loop in the graph. Make sure that all the data is entered correctly. Loops cannot be part of a minimal spanning tree, so enter the data ignoring all loops in the graph.</p>
+% end
+<!-- ошибка наличия повторов при вводе ребер -->
+% if "povtor" in errors:
+<p>In the introduced graph there is a repetition of the same edge. Check the correctness of the entered data and try again.</p>
+% end
+% end
+<!-- вывод результата при правильном вводе данных -->
 % if len(errors) == 0:
 <p class="hhh">Solution by Kruslak 's algorithm</p>
 <p> <label>Source data (make sure all data has been entered correctly): </label></p>
+<!-- вывод данных, которые ввел пользователь -->
 <p><table align = center cellspacing="3" border="1" cellpadding="5">
     <tr>
             <td>weight</td>
@@ -24,7 +42,9 @@
         </tr>
         % end
     </table></p>
-    <p> <label>The first step to find the minimum spanning tree is to sort the entered data in ascending order of the edge weight</label></p>
+    <!-- описание первого этапа алгоритма -->
+    <p> <label>The first step to find the minimum spanning tree is to sort the entered data in ascending order of the edge weight.</label></p>
+    <!-- вывод результата сортировки графа -->
     <p><table align = center cellspacing="3" border="1" cellpadding="5">
     <tr>
             <td>weight</td>
@@ -39,7 +59,31 @@
         </tr>
         % end
     </table></p>
+    <!-- вывод поэтапного пояснения действия алгоритма -->
+    % i = 0
+    % for pr in process:
+    <p><label>{{pr}}</label></p>
+    % if "part of the spanning tree" in pr:
+    % i += 1
+    <table align = center cellspacing="3" border="1" cellpadding="5">
+    <tr>
+            <td>weight</td>
+            <td>sart</td>
+            <td>final</td>
+        </tr>
+        % for j in range(i):
+        <tr>
+            % for value in T[j]:
+            <td>{{ value }}</td>
+            % end
+        </tr>
+        % end
+    </table></p>
 
+    %end
+    % end
+    <!-- вывод итогового ответа -->
+    <p class="hhh">The result of the search for the minimum spanning tree by the Kruskal method: </p>
 <p><table align = center cellspacing="3" border="1" cellpadding="5">
     <tr>
             <td>weight</td>
@@ -56,6 +100,8 @@
     </table></p>
     % end
     </form>
+    <form action="/kruskal">
+   <p class="hhh"><input type="submit" value="Back"></p>
 </body>
 </div>
 </html>
